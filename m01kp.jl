59a13d34a1c5dd3592f13800355083a9a1506eef
ducanh-le <lub.the.studio@gmail.com>
2021-12-21 10:41:17 +0100

    finish lower bound

--- m01kp.jl
+++ m01kp.jl
@@ -66,8 +66,8 @@ end
 function bound_and_bound(p,w,c)
     n = length(p)
     m = length(c)
-    b = Array{Int,1}(undef,n)  #value = 1 if item wasn't inserted in knapsacks 1..i, 0 otherwise
-    fill!(b,1)
+    x_history = Array{Int,2}(undef,m,n); fill!(x_history,0) #matrix whose rows give the solutions found in the last application of lower_bound
+    b = Array{Int,1}(undef,n); fill!(b,1)  #value = 1 if item wasn't inserted in knapsacks 1..i, 0 otherwise
     k = copy(c)   #available capacities of each knapsacks
     x_current = Array{Int,2}(undef,m-1,n); fill!(x_current,0) #current solution
     S = Array{Int,2}(undef,m-1,n); fill!(S,0) # S[q,j] pointer to the item inserted in knapsack q just before item j
@@ -80,19 +80,78 @@ end
 
 #solve the current relaxation surrogate problem to find the upper bound
 function upper_bound(p,w,c,i,k,b,z_current)
-    p_new = Int[]
-    w_new = Int[]
-    for j in 1:length(b)
+    p_prime = Int[]
+    w_prime = Int[]
+    for j in 1:n
         if b[j] == 1    #item wasn't inserted in any knapsacks
-            append!(p_new,p[j])
-            append!(w_new,w[j])
+            append!(p_prime,p[j])
+            append!(w_prime,w[j])
         end
     end
     c_new = k[i] + sum(c[j] for j in i+1:length(c))
-    x_new, z_new = resolution_v2(p_new,w_new,c_new)
-    return z_current + z_new
+    x_prime, z_prime = resolution_v2(p_prime,w_prime,c_new)
+    return z_current + z_prime
 end
 
-function lower_bound()
+#glouton heuristic to find the lower bound
+function lower_bound(p,w,i,k,b,z_current,S,x_history)
+    m = length(k); n = length(b)
+#create N'
+    p_prime, w_prime, index_prime = Int[], Int[], Int[]
+    for j in 1:n
+        if b[j] == 1    #item wasn't inserted in any knapsacks
+            append!(p_prime,p[j])
+            append!(w_prime,w[j])
+            append!(index_prime,j)   #original index item for N'
+        end
+    end
+#create N_u
+    p_u, w_u, index_u = Int[], Int[], Int[]
+    for j in 1:length(index_prime)
+        if S[i,index_prime[j]] == 0
+            append!(p_u,p_prime[j])
+            append!(w_u,w_prime[j])
+            append!(index_u,index_prime[j])   #original index item for N_u
+        end
+    end
 
+    u = i
+    keep_going = true
+    while keep_going
+    #fix all item which can't be inserted in knapsack u to 0
+        all_item = 1:n
+        not_u_item = setdiff(all_item, index_u)
+        for j in not_u_item
+            x_history[u,j] = 0
+        end
+    #fill this knapsack u
+        if length(index_u) != 0
+            x_u, z_u = resolution_v2(p_u,w_u,k[u])
+            for j in 1:length(x_u)
+                x_history[u,index_u[j]] = x_u[j]
+            end
+            z_current += z_u
+        end
+    
+        if (u == m)
+            return z_current
+        else
+        #update N'
+            for j in index_u
+                if x_history[u,j] == 1
+                    setdiff!(index_prime,j)
+                end
+            end
+            p_prime, w_prime = Int[], Int[]
+            for j in index_prime
+                append!(p_prime,p[j])
+                append!(w_prime,w[j])
+            end
+        #update N_u
+            p_u = copy(p_prime)
+            w_u = copy(w_prime)
+        
+            u += 1
+        end
+    end
 end
